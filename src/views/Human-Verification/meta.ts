f73f2f7b4fd454540a1aa4c67630b18d974fd54e
JUNO_OKYO <junookyo@gmail.com>
2026-03-12 00:51:15 +0700

    fix: add missing category & PageMeta type to page metas
    - Dev-RPG: add category 'game', add PageMeta type
    - Human-Verification: add category 'fun', remove invalid 'order' field
    - csgo-case-opening, project-42, qrcode-generator: add PageMeta type

--- src/views/Human-Verification/meta.ts
+++ src/views/Human-Verification/meta.ts
@@ -1,7 +1,11 @@
-export default {
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
   name: 'Human Verification',
   description: 'Chứng minh bạn là một thực thể sinh học có tri giác.',
   author: 'h1n4mx0',
   facebook: 'https://github.com/h1n4mx0',
-  order: 3,
+  category: 'fun',
 }
+
+export default meta
--- src/views/qrcode-generator/meta.ts
+++ src/views/qrcode-generator/meta.ts
@@ -1,7 +1,11 @@
-export default {
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
   name: 'QR Code Generator',
   description: 'Professional QR generator with custom styling and frame support',
   author: 'ductongnguyen',
   facebook: 'https://www.facebook.com/it.tongnd/',
   category: 'tool',
 }
+
+export default meta
