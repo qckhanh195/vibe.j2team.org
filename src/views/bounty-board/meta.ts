09737d3b5becdc0af0170f6bdb638b9c50da1b61
Quoc Khanh <qckhanh205@gmail.com>
2026-03-22 01:48:39 +0700

    fix: fix meta

--- src/views/bounty-board/meta.ts
+++ src/views/bounty-board/meta.ts
@@ -2,7 +2,7 @@ import type { PageMeta } from '@/types/page'
 
 const meta: PageMeta = {
   name: 'Bounty Board',
-  description: '[Hệ thống thông báo] Kích hoạt nhiệm vụ đời thực',
+  description: '[Hệ thống gửi thông báo]: Kích hoạt nhiệm vụ đời thực',
   author: 'Quoc Khanh',
   facebook: 'https://www.facebook.com/qckhanh.195/',
   category: 'game',
